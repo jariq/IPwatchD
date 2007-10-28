1fb7e96f103ee668e955d37455869925ff319db1
jariq <jariq@9552bee1-e631-0410-8bfb-d91fd4c5cdef>
2007-10-28 08:56:58 +0000

    General code cleanup Should also compile with gcc3 cleanly now

--- current/src/daemonize.c
+++ current/src/daemonize.c
@@ -26,67 +26,67 @@ extern char  msgbuf[IPWD_MSG_BUFSIZ];  /* Buffer for output messages */
 /* ipwd_daemonize - daemonize proccess */
  
 int ipwd_daemonize(void) {
-     
-  /* If parent of this process is init we are already in daemon mode */ 
-  if (getppid() == 1) {
-    snprintf(msgbuf, IPWD_MSG_BUFSIZ, "Already running as daemon.\n");
-    ipwd_message(msgbuf, IPWD_MSG_INFO);
-    return(IPWD_RV_SUCCESS);
-  }
-
-  /* Fork child process */
-  pid_t pid = fork();
-  if (pid < 0) {
-    snprintf(msgbuf, IPWD_MSG_BUFSIZ, "Could not fork child process.\n");
-    ipwd_message(msgbuf, IPWD_MSG_ERROR);
-    return(IPWD_RV_ERROR);
-  }
-  
-  /* Fork was successful we can exit parent */  
-  if (pid > 0) {
-    exit(IPWD_RV_SUCCESS);
-  }
-
-  /* Set default umask */
-  umask(0);
-
-  /* Create new session */
-  pid_t sid = setsid();
-  if (sid < 0) {
-    snprintf(msgbuf, IPWD_MSG_BUFSIZ, "Could not create new session.\n");
-    ipwd_message(msgbuf, IPWD_MSG_ERROR);
-    return(IPWD_RV_ERROR);
-  }
-
-  /* Change current directory to root */
-  if ((chdir("/")) == -1) {
-    snprintf(msgbuf, IPWD_MSG_BUFSIZ, "Unable to change current directory to /.\n");
-    ipwd_message(msgbuf, IPWD_MSG_ERROR);
-    return(IPWD_RV_ERROR);
-  }
-
-  /* Redirect standard input */
-  if ((freopen( "/dev/null", "r", stdin)) == NULL) {
-    snprintf(msgbuf, IPWD_MSG_BUFSIZ, "Unable to redirect STDIN.\n");
-    ipwd_message(msgbuf, IPWD_MSG_ERROR);
-    return(IPWD_RV_ERROR);
-  }
-
-  /* Redirect standard output */    
-  if ((freopen( "/dev/null", "w", stdout)) == NULL) {
-    snprintf(msgbuf, IPWD_MSG_BUFSIZ, "Unable to redirect STDOUT.\n");
-    ipwd_message(msgbuf, IPWD_MSG_ERROR);
-    return(IPWD_RV_ERROR);
-  }
-
-  /* Redirect standard error output */
-  if ((freopen( "/dev/null", "w", stderr)) == NULL) {
-    snprintf(msgbuf, IPWD_MSG_BUFSIZ, "Unable to redirect STDERR.\n");
-    ipwd_message(msgbuf, IPWD_MSG_ERROR);
-    return(IPWD_RV_ERROR);
-  }
-
-  return(IPWD_RV_SUCCESS);
-  
+	
+	/* If parent of this process is init we are already in daemon mode */ 
+	if (getppid() == 1) {
+		snprintf(msgbuf, IPWD_MSG_BUFSIZ, "Already running as daemon");
+		ipwd_message(msgbuf, IPWD_MSG_INFO);
+		return(IPWD_RV_SUCCESS);
+	}
+	
+	/* Fork child process */
+	pid_t pid = fork();
+	if (pid < 0) {
+		snprintf(msgbuf, IPWD_MSG_BUFSIZ, "Unable to fork a child process");
+		ipwd_message(msgbuf, IPWD_MSG_ERROR);
+		return(IPWD_RV_ERROR);
+	}
+	
+	/* Fork was successful we can exit parent */  
+	if (pid > 0) {
+		exit(IPWD_RV_SUCCESS);
+	}
+	
+	/* Set default umask */
+	umask(0);
+	
+	/* Create new session */
+	pid_t sid = setsid();
+	if (sid < 0) {
+		snprintf(msgbuf, IPWD_MSG_BUFSIZ, "Unable to create a new session");
+		ipwd_message(msgbuf, IPWD_MSG_ERROR);
+		return(IPWD_RV_ERROR);
+	}
+	
+	/* Change current directory to root */
+	if ((chdir("/")) == -1) {
+		snprintf(msgbuf, IPWD_MSG_BUFSIZ, "Unable to change current directory to /");
+		ipwd_message(msgbuf, IPWD_MSG_ERROR);
+		return(IPWD_RV_ERROR);
+	}
+	
+	/* Redirect standard input */
+	if ((freopen( "/dev/null", "r", stdin)) == NULL) {
+		snprintf(msgbuf, IPWD_MSG_BUFSIZ, "Unable to redirect STDIN");
+		ipwd_message(msgbuf, IPWD_MSG_ERROR);
+		return(IPWD_RV_ERROR);
+	}
+	
+	/* Redirect standard output */
+	if ((freopen( "/dev/null", "w", stdout)) == NULL) {
+		snprintf(msgbuf, IPWD_MSG_BUFSIZ, "Unable to redirect STDOUT");
+		ipwd_message(msgbuf, IPWD_MSG_ERROR);
+		return(IPWD_RV_ERROR);
+	}
+	
+	/* Redirect standard error output */
+	if ((freopen( "/dev/null", "w", stderr)) == NULL) {
+		snprintf(msgbuf, IPWD_MSG_BUFSIZ, "Unable to redirect STDERR");
+		ipwd_message(msgbuf, IPWD_MSG_ERROR);
+		return(IPWD_RV_ERROR);
+	}
+	
+	return(IPWD_RV_SUCCESS);
+	
 }
 
--- current/src/ipwatchd.h
+++ current/src/ipwatchd.h
@@ -32,40 +32,41 @@
 #include <pcap.h>
 
 
-#define IPWATCHD_VERSION "IPwatchD 1.0"
+#define IPWATCHD_VERSION "IPwatchD 1.0.1"
 
 
 /* Global return values */
-#define IPWD_RV_SUCCESS 0         /* Return Value Success */
-#define IPWD_RV_ERROR   2         /* Return Value Error */
+#define IPWD_RV_SUCCESS 0		/* Return Value Success */
+#define IPWD_RV_ERROR   2		/* Return Value Error */
 
 
 /* Messages */
-#define IPWD_MSG_BUFSIZ  1000     /* Size of buffer for output messages */
-#define IPWD_MSG_INFO    1        /* Message type: information */
-#define IPWD_MSG_ERROR   2        /* Message type: error */
-#define IPWD_MSG_ALERT   3        /* Message type: alert */
+#define IPWD_MSG_BUFSIZ  1000		/* Size of buffer for output messages */
+#define IPWD_MSG_INFO    1		/* Message type: information */
+#define IPWD_MSG_ERROR   2		/* Message type: error */
+#define IPWD_MSG_ALERT   3		/* Message type: alert */
+#define IPWD_MSG_DEBUG   4		/* Message type: debug */
 
 
 /* Modes */
-#define IPWD_MODE_ACTIVE  1       /* IPwatchD should operate in active mode */
-#define IPWD_MODE_PASSIVE 2       /* IPwatchD should operate in passive mode */
+#define IPWD_MODE_ACTIVE  1		/* IPwatchD should operate in active mode */
+#define IPWD_MODE_PASSIVE 2		/* IPwatchD should operate in passive mode */
 
 /* Structure that holds information about ONE network interface */
 typedef struct {
-
-  char device[10];  /* Device name */
-  char ip[20];      /* IP address of device */
-  char mac[20];     /* MAC address of device */
-  int mode;         /* IPwatch mode on interface: IPWATCHD_DEVICE_MODE_ACTIVE or IPWATCHD_DEVICE_MODE_PASSIVE */
-
+	
+	char device[10];		/* Device name */
+	char ip[20];			/* IP address of device */
+	char mac[20];			/* MAC address of device */
+	int mode;			/* IPwatch mode on interface: IPWATCHD_DEVICE_MODE_ACTIVE or IPWATCHD_DEVICE_MODE_PASSIVE */
+	
 } IPWD_S_DEV;
 
 /* Structure that holds information about ALL network interfaces */
 typedef struct {
 	
-	IPWD_S_DEV *dev;   /* Dynamicaly allocated array of IPWD_S_DEV structures */
-	int devnum;        /* Number of watched interfaces */
+	IPWD_S_DEV *dev;		/* Dynamicaly allocated array of IPWD_S_DEV structures */
+	int devnum;			/* Number of watched interfaces */
 	
 } IPWD_S_DEVS;
 
@@ -77,11 +78,11 @@ typedef struct {
 
 /* Structure for parsing of addresses from packet */
 typedef struct {
-
-	u_int8_t arp_sha[ETH_ALEN];	 /* Source MAC address */
-	u_int8_t arp_spa[4];		 /* Source IP address */
-	u_int8_t arp_tha[ETH_ALEN];	 /* Destination MAC address */
-	u_int8_t arp_tpa[4];		 /* Destination IP address */
+	
+	u_int8_t arp_sha[ETH_ALEN];	/* Source MAC address */
+	u_int8_t arp_spa[4];		/* Source IP address */
+	u_int8_t arp_tha[ETH_ALEN];	/* Destination MAC address */
+	u_int8_t arp_tpa[4];		/* Destination IP address */
 	
 } IPWD_S_ARP_HEADER;
 
@@ -105,9 +106,12 @@ int ipwd_devinfo(char *p_dev, char *p_ip, char *p_mac);
 int ipwd_genarp(char *dev, char *p_sip, char *p_smac, char *p_dip, char *p_dmac, int opcode);
 
 /* ipwatchd.c */
-void ipwd_signal_handler(int signal);
 void ipwd_print_help(void);
 
 /* message.c */
 void ipwd_message(char *msg, int type);
+
+/* signal.c */
+int ipwd_set_signal_handler(void);
+void ipwd_signal_handler(int signal);
 
